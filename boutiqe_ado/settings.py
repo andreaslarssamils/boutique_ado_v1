4d1f4a26cdf8efa0064189029dcc9f32c99ff0a5
andreaslarssamils <larssamils.andreas@gmail.com>
2025-11-10 18:42:46 +0100

    Added bg context and delivery logic

--- boutiqe_ado/settings.py
+++ boutiqe_ado/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",  # important required by allauth
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "bag.contexts.bag_contents",  # custom context processor
             ],
         },
     },
@@ -159,6 +160,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
