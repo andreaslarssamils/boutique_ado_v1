6d018e6004a685cd975be42202213813bfd2ad91
andreaslarssamils <larssamils.andreas@gmail.com>
2026-01-18 12:09:37 +0100

    add original_bag and stripe_pid fields to Order model; update checkout view and webhook handler for Stripe integration

--- checkout/views.py
+++ checkout/views.py
@@ -56,7 +56,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get("client_secret").split("_secret")[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -112,6 +116,8 @@ def checkout(request):
 
         order_form = OrderForm()
 
+        # in the video, the below code is not indented properly
+        # this is the correct indentation
         if not stripe_public_key:
             messages.warning(
                 request,
@@ -127,6 +133,7 @@ def checkout(request):
         }
 
         return render(request, template, context)
+        # end of the corrected indentation
 
 
 def checkout_success(request, order_number):
