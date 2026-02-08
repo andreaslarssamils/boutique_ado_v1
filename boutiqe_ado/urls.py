6188235462a2c180bc24ec36033bb4ac30f39ca6
andreaslarssamils <larssamils.andreas@gmail.com>
2026-02-08 17:07:21 +0100

    feat: implement custom 404 error page and update email verification setting

--- boutiqe_ado/urls.py
+++ boutiqe_ado/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for boutiqe_ado project.
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,6 +19,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import handler404
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
@@ -29,3 +30,4 @@ urlpatterns = [
     path("checkout/", include("checkout.urls")),
     path("profile/", include("profiles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = "boutique_ado.views.handler404"
